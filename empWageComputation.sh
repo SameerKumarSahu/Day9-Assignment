fa5569a8aae8855a3fab96a32cdb7572f91df07f
SameerKumarSahu <sameerkumarsahu0@gmail.com>
2021-06-27 22:46:39 +0530

    Check Availablity

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,16 @@
 #! /bin/bash -x
 
 echo "Welcome Message"
+
+isPresent=1
+isAbsent=0
+checkAvail=$((RANDOM%2))
+
+
+if [[ $isPresent -eq $checkAvail ]]
+then
+        echo "Employee is Present"        
+else
+        echo "Employee is Absent"
+
+fi
