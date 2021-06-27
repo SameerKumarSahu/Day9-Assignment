d3ad52b11f4f7e56ee240fa8014e8d067c30dd25
SameerKumarSahu <sameerkumarsahu0@gmail.com>
2021-06-27 22:51:24 +0530

    Daily wages of Emp

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,12 +4,16 @@ echo "Welcome Message"
 
 isPresent=1
 isAbsent=0
+empWagePerHrs=20
+empDayHrs=8
 checkAvail=$((RANDOM%2))
 
 
 if [[ $isPresent -eq $checkAvail ]]
 then
-        echo "Employee is Present"        
+	echo "Employee is Present"
+	totalSalary=$(( $empWagePerHrs * $empDayHrs ))
+        echo "Total Salary of emp is :" $totalSalary
 else
         echo "Employee is Absent"
 
