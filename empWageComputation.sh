265c981d21baeac881579147157e06233d590576
SameerKumarSahu <sameerkumarsahu0@gmail.com>
2021-06-27 23:01:22 +0530

    Adding part time wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -18,3 +18,24 @@ else
         echo "Employee is Absent"
 
 fi
+
+echo "===="
+
+
+isPartTime=1;
+isFullTime=2;
+empRatePerHr=20;
+randomCheck=$((RANDOM%3));
+
+if [ $isFullTime -eq $randomCheck ];
+then
+        empHrs=8;
+elif [ $isPartTime -eq $randomCheck ];
+then
+        empHrs=4;
+else
+        empHrs=0;
+fi
+
+salary=$(($empHrs*empRatePerHr));
+echo "Salary of employee is "$salary
