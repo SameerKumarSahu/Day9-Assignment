0d8c75100bc1bd4ec5d2b6014cfba4ac3bd2d91e
SameerKumarSahu <sameerkumarsahu0@gmail.com>
2021-06-27 23:15:05 +0530

    Wages till a condition

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -77,3 +77,31 @@ do
                 totalSalary=$(($totalSalary+$salary));
 done
 echo "monthly salary of employee is "$totalSalary
+
+
+maxHrsInMonth=100;
+totalEmpHrs=0;
+totalWorkingDays=0;
+
+while [[ $totalEmpHrs -lt $maxHrsInMonth &&
+         $totalWorkingDays -lt $numWorkingDays ]]
+do
+        ((totalWorkingDays++))
+        empCheck=$((RANDOM%3));
+        case $empCheck in
+                $isFullTime)
+                       empHrs=8
+                       ;;
+                $isPartTime)
+                       empHrs=4
+                       ;;
+                *)
+                       empHrs=0
+                       ;;
+         esac
+         totalEmpHrs=$(($totalEmpHrs+$empHrs));
+
+done
+
+totalSalary=$(($totalEmpHrs*$empRatePerHr));
+echo "monthly salary of employee is "$totalSalary
